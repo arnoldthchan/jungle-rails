921ce61eac52efd8058cc13f6333cf34bef9f147
Arnold Chan <arnoldthchan@gmail.com>
2017-08-02 20:47:06 +0000

    test lines commented out

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,9 @@ class ProductsController < ApplicationController
 
   def index
     @products = Product.all.order(created_at: :desc)
+    # @products = Product.where('price_cents > ?', 10000).order(created_at: :desc)
+    # raise
+    # byebug
   end
 
   def show
