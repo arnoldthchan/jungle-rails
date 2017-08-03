d1c4e31991a9e8798e2a6ab4018a3d98368baf0d
Arnold Chan <arnoldthchan@gmail.com>
2017-08-03 22:15:08 +0000

    removed admin user and password, env instead

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,4 @@
 class Admin::BaseController < ApplicationController
-  http_basic_authenticate_with name: "jungle", password: "book", except: :root
+  http_basic_authenticate_with name: ENV["HTTPS_NAME"], password: ENV["HTTPS_PASSWORD"], except: :root
 
 end
