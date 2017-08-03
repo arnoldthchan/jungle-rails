99b58bea5742c934f2abd5c14dc1112aa7394bbc
Arnold Chan <arnoldthchan@gmail.com>
2017-08-03 22:17:25 +0000

    changed .envs around

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,4 +1,4 @@
 class Admin::BaseController < ApplicationController
-  http_basic_authenticate_with name: ENV["HTTPS_NAME"], password: ENV["HTTPS_PASSWORD"], except: :root
+  http_basic_authenticate_with name: ENV['HTTPS_NAME'], password: ENV['HTTPS_PASSWORD'], except: :root
 
 end
