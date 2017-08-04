14651a4f1b0310ec70e4f61b97cdf17dc111b597
Arnold Chan <arnoldthchan@gmail.com>
2017-08-04 14:49:04 +0000

    Updated schema and added review seeds

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,7 @@
 class Review < ActiveRecord::Base
+  # belongs_to :TEST
+  validates :product_id, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
+  validates_inclusion_of :rating, :in => 1..5
 end
