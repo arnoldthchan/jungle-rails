c239976647ec7fa60be07907d325a4192c88c42d
Arnold Chan <arnoldthchan@gmail.com>
2017-08-04 20:40:06 +0000

    fixed user reviews posting, as well as updated models

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,15 +1,22 @@
 class ReviewsController < ApplicationController
 
   def create
-    @review = Review.new(review_params)
-    @review.product_id = params[:product_id]
-    @review.user_id = current_user
+
+    @review = Review.new(
+    product_id: params["product_id"],
+    user_id: current_user.id,
+    description: review_params[:description],
+    rating: review_params[:rating]
+    )
 
     if @review.save
-      redirect_to :root
+      redirect_to product_path (@review.product), notice: 'Review posted!'
     else
-      redirect_to :root
+      redirect_to product_path (@review.product), notice: 'Review failed to post!'
     end
+  end
+
+  def destroy
   end
 
   private
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,6 @@
 class Review < ActiveRecord::Base
-  # belongs_to :TEST
+  belongs_to :user
+  belongs_to :product
   validates :product_id, presence: true
   validates :user_id, presence: true
   validates :description, presence: true
