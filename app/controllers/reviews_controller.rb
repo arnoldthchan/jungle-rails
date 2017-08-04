d29f360df18a523b541c5d8da9dd76712632671b
Arnold Chan <arnoldthchan@gmail.com>
2017-08-04 21:44:21 +0000

    Added destroy method for comments owned by current user

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-
+  before_filter :check_login
   def create
 
     @review = Review.new(
@@ -10,16 +10,23 @@ class ReviewsController < ApplicationController
     )
 
     if @review.save
-      redirect_to product_path (@review.product), notice: 'Review posted!'
+      redirect_to product_path (params[:product_id]), notice: 'Review posted!'
     else
-      redirect_to product_path (@review.product), notice: 'Review failed to post!'
+      redirect_to product_path (params[:product_id]), notice: 'Review failed to post!'
     end
   end
 
   def destroy
+    @review = Review.find params[:id]
+    @review.destroy
+    redirect_to product_path (params[:product_id]) , notice: 'Review deleted!'
   end
 
   private
+
+  def check_login
+    redirect_to product_path (params[:product_id]) if current_user == nil
+  end
 
   def review_params
     params.require(:review).permit(:rating, :description)
